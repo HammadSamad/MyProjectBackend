6dd11345f78a5722a82098cf3e8a74e80bf93c2b
HammadSamad <154352602+HammadSamad@users.noreply.github.com>
2026-02-03 09:04:19 +0500

    file update
    file update

--- Backend-Api/Models/Order.cs
+++ Backend-Api/Models/Order.cs
@@ -15,8 +15,6 @@ public partial class Order
 
     public string OrderStatus { get; set; } = null!;
 
-    public Shipment? Shipment { get; set; }
-
     public DateTime? CreatedAt { get; set; }
 
     public DateTime? UpdatedAt { get; set; }
--- Backend-Api/obj/Debug/net9.0/Backend-Api.AssemblyInfo.cs
+++ Backend-Api/obj/Debug/net9.0/Backend-Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend-Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54b3b44bc35a3e35f732a83c0f6393aa6e2d2d9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4962196c4b59ef16cd216672e63247cb0835a573")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend-Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend-Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
